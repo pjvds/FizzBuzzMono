9c41d431109a78e61eff4031ed4e246f78e1f5f1
pjvds <pj@born2code.net>
2012-11-17 15:02:47 +0100

    Write numbers 1 … 100

--- FizzBuzzMono/Main.cs
+++ FizzBuzzMono/Main.cs
@@ -6,7 +6,10 @@ namespace FizzBuzzMono
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 100; i++)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
         }
     }
 }
