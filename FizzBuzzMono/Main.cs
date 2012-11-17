c1d1cc2b02cd781886128a7e8623ce2aacee49a0
pjvds <pj@born2code.net>
2012-11-17 15:05:42 +0100

    Write Buzz for numbers dividable by 5

--- FizzBuzzMono/Main.cs
+++ FizzBuzzMono/Main.cs
@@ -10,6 +10,8 @@ namespace FizzBuzzMono
             {
                 if(i % 3 == 0)
                     Console.WriteLine("Fizz");
+                else if(i % 5 == 0)
+                    Console.WriteLine("Buzz");
                 else
                     Console.WriteLine(i);
             }
