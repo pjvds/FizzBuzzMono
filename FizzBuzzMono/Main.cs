fff53d1bd57e01654bf1c07ef3a5bbcba7d8a7b3
pjvds <pj@born2code.net>
2012-11-17 15:04:55 +0100

    Write Fizz for numbers dividable by 3

--- FizzBuzzMono/Main.cs
+++ FizzBuzzMono/Main.cs
@@ -7,7 +7,12 @@ namespace FizzBuzzMono
         public static void Main(string[] args)
         {
             for(int i = 1; i <= 100; i++)
-                Console.WriteLine(i);
+            {
+                if(i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
 
             Console.ReadKey();
         }
